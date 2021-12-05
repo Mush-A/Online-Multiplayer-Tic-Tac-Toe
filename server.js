4f82bd612c3b8a030e81d15c19b29a105067c4e7
Musharaf Altaf <mushrafaltaf@gmail.com>
2021-12-05 13:26:04 +0530

    required dotenv

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
